51413d924c8a731c5b4d0c30350fe24d33ae317d
Nerdobyte <nadiafernandes@live.com>
2025-12-02 23:03:33 +0000

    Update Firebase config and app logic

--- app.py
+++ app.py
@@ -544,7 +544,7 @@ with st.sidebar:
     with st.form("dev_feedback_form"):
         st.text_area(
             "", 
-            placeholder="One thing I’d change is...",
+            placeholder="One thing I’d change...",
             height=100,
             key="idea_note_input"
         )
